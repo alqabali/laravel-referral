60c4d76834cf1b332696211156042f737ad36904
alqabali <marsasoft@gmail.com>
2021-08-05 03:15:19 +0100

    Add files via upload

--- database/migrations/create_affiliate_referrals_table.php
+++ database/migrations/create_affiliate_referrals_table.php
@@ -13,18 +13,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAffiliateProgramsTable extends Migration
+class CreateAffiliateReferralsTable extends Migration
 {
     /**
      * Run the migrations.
      */
     public function up()
     {
-        Schema::create('affiliateprograms', function (Blueprint $table) {
+        Schema::create('referrals', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('slug')->unique();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('affiliate_program_id')->nullable();
+            $table->string('status');
+            $table->integer('bounty')->nullable();
             $table->timestamps();
+
+            $table->foreign('user_id')
+            ->references('id')->on('users')
+            ->onDelete('cascade');
+
+            $table->foreign('affiliate_program_id')
+            ->references('id')->on('affiliate_programs')
+            ->onDelete('cascade');
         });
     }
 
